8aae5c5112fcf609af94d2f213165c2852b3f40a
ngam <67342040+ngam@users.noreply.github.com>
2022-10-28 18:54:59 -0400

    download files

---
+++ exigence/util/file.py
@@ -0,0 +1,20 @@
+def read_lines_from_file(filename, strip=True):
+    with open(filename, 'r') as fname:
+        raw_lines = fname.readlines()
+    if strip:
+        return [line.strip() for line in raw_lines]
+    else:
+        return raw_lines
+
+def download_files_from_url(url, dest):
+    import urllib
+    import urllib.request
+    urllib.request.urlretrieve(url, dest)
+
+def download_files_from_url_batch(urls, dest_folder):
+    import os
+    if not os.path.exists(f"{dest_folder}"):
+        os.makedirs(f"{dest_folder}", exist_ok=True)
+    for url in urls:
+        filename = url.split('/')[-1]
+        download_files_from_url(url, os.path.join(dest_folder, filename))
